be9b18747766339d41fb8658e4018f30bfd2c967
EC2 Default User <ec2-user@ip-172-31-39-132.ap-northeast-3.compute.internal>
2022-03-17 16:22:55 +0000

    end

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,7 +14,5 @@ class Order < ApplicationRecord
     item.with_tax_price * amount
     end
     
-    enum status: {  waiting_for_deposit: 0, payment_confirmation: 1, in_production: 2, preparing_to_ship: 3, sent: 4 }
-    
     enum payment_method: { credit_card: 0, transfer: 1 }
 end
